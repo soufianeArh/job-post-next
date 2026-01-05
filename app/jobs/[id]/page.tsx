01d8fd3f14a4dc8971620e4ddd4bd9c0989c55d9
souf <arhlabi.soufiane02@gmail.com>
2026-01-05 16:03:46 +0800

    browse job filter applied

---
+++ app/jobs/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function SingleJob(){
+      return (
+            <div>
+                  
+            </div>
+      )
+}
--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -3,31 +3,57 @@
 //serverside componenet could directly targett the db but not preferred i guess
 import JobCard from "@/component/JobCard";
 import {prisma} from "@/lib/prisma";
+import { redirect } from 'next/navigation'
+import { FormEvent } from "react";
 
-export default async function Jobs() {
+
+export default async function Jobs(
+  {searchParams}:{searchParams:Promise<{[key:string]: string | string[] | undefined}>
+}) {
   //target db directly
-  let jobs
+  const {q, type, location} = await searchParams;
+  // const query = q as string | undefined
+  
+  console.log(q, type, location)
+  let jobs;
   try{
-       jobs = await prisma.job.findMany({
-            orderBy:{postedAt:'desc'}, include:{user:true}
-      })
+    jobs = await prisma.job.findMany({
+      where:{
+        AND:[
+          type? {type:type as string}:{},
+          location? {location: location as string} : {},
+          q? {OR:[
+            {title:{contains:q as string,}},
+            {description:{contains:q as string,}},
+            {company:{contains:q as string,}}
+          ]}:{}
+        ]
+      },
+      orderBy: { postedAt: 'desc' },
+      include: { user: true }
+    })
   }catch(err){
       console.log(err)
-      alert("Network error: ");
+      // alert("Network error: ");
   }
+ console.log("found jobs ", {q, type, location}, jobs)
 
   return (
     <div>
-      <div className="flex flex-col md:flex-row gap-4 p-4 bg-gray-100 rounded-lg max-w-4xl mx-auto">
+       <form method="GET">
+        <div className="flex flex-col md:flex-row gap-4 p-4 bg-gray-100 rounded-lg max-w-4xl mx-auto">
         {/* Job Title */}
         <input
           type="text"
+          name="q"
           placeholder="Search jobs"
           className="flex-1 p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
 
         {/* Job Type */}
-        <select className="flex-1 p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
+        <select 
+        name="type"
+        className="flex-1 p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
           <option value="">All Types</option>
           <option value="full-time">Full-time</option>
           <option value="part-time">Part-time</option>
@@ -37,10 +63,17 @@ export default async function Jobs() {
         {/* Location */}
         <input
           type="text"
+          name="location"
           placeholder="Location"
           className="flex-1 p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
-      </div>
+        </div>
+        <button
+        type="submit"
+        className="wx-full bg-indigo-600 rounded-md text-xl font-semibold text-white hover:bg-indigo-700 ">
+            Search
+        </button>
+      </form>
       <div className="mt-4">
             {jobs?.map(job=>
                    <JobCard
